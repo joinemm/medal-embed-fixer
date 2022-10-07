696abca6572889c7d12508cd94c59d7e4c823a09
Joinemm <joonas@rautiola.co>
2022-10-07 14:29:03 +0300

    :ambulance: Extract current build id from NEXT_DATA
    allows the bot to keep up with build changes and keep working

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+import asyncio
+import json
 import os
 import re
+import sys
 
 import aiohttp
 import discord
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+async def get_medal_clip(clip_path: str) -> dict:
+    original_url = f"https://medal.tv/games/{clip_path}"
+    async with aiohttp.ClientSession() as session:
+        # get page source to extract __NEXT_DATA__
+        async with session.get(original_url) as response:
+            source = await response.text()
+            next_data = json.loads(
+                BeautifulSoup(source, "lxml").find("script", {"id": "__NEXT_DATA__"}).text
+            )
+
+        # get the build id from next_data
+        build_id = next_data["buildId"]
+
+        # get clip data from newest build
+        async with session.get(
+            f"https://medal.tv/_next/data/{build_id}/en/games/{clip_path}.json"
+        ) as response:
+            return await response.json()
 
 
 class MyClient(discord.Client):
@@ -20,27 +44,29 @@ class MyClient(discord.Client):
         regex = r"https://medal\.tv/games/([^\s?]*)"
         matches = re.finditer(regex, message.content)
         for match in matches:
-            clip_path = match.group(1)
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                    f"https://medal.tv/_next/data/ELKh_LJ4D0_htfA9kVKKA/en/games/{clip_path}.json"
-                ) as response:
-                    data = await response.json()
+            data = await get_medal_clip(match.group(1))
 
             content_url = data["pageProps"]["clip"]["contentUrl"]
             content_title = data["pageProps"]["clip"]["contentTitle"]
+
             await message.reply(
                 f":magic_wand: **{content_title}** {content_url}", mention_author=True
             )
+
             print(f"Fixed link for {message.author} : {message.content}")
+
             try:
+                await asyncio.sleep(1)
                 await message.edit(suppress=True)
             except discord.errors.Forbidden:
                 print("Tried to suppress message but no permissions :(")
 
 
-intents = discord.Intents.default()
-intents.message_content = True
+if sys.argv[0] == "test":
+    asyncio.run(get_medal_clip("valorant/clips/DC4NEq0hN_KNe/d1337QdxUXRo"))
+else:
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(os.environ.get("TOKEN"))
+    client = MyClient(intents=intents)
+    client.run(os.environ.get("TOKEN"))
