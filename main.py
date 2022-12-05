3666f7fd3ab23d96c971566ea2b8d3ceedd9480c
Joinemm <joonas@rautiola.co>
2022-12-05 22:50:46 +0200

    rewrite for new medal api

--- main.py
+++ main.py
@@ -1,85 +1,76 @@
 import asyncio
-import json
+from dataclasses import dataclass
 import os
 import re
 import sys
 
 import aiohttp
 import discord
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-async def get_medal_clip(clip_path: str) -> dict | None:
-    original_url = f"https://medal.tv/games/{clip_path}"
+@dataclass
+class MedalClip:
+    title: str
+    url: str
+
+
+def find_content_ids(text: str):
+    regex = r"https://medal\.tv/games/(\S*?)/clips/([^\s?]*)/"
+    matches = re.finditer(regex, text)
+    return [match.group(2) for match in matches]
+
+
+async def api_content(content_id: str):
+    api_url = f"https://medal.tv/api/content/{content_id}"
     async with aiohttp.ClientSession() as session:
-        # get page source to extract __NEXT_DATA__
-        async with session.get(original_url) as response:
-            source = await response.text()
-            next_data_script = BeautifulSoup(source, "lxml").find(
-                "script", {"id": "__NEXT_DATA__"}
-            )
-            if next_data_script is None:
-                return None
-
-        next_data = json.loads(next_data_script.text)
-
-        # get the build id from next_data
-        build_id: str = next_data["buildId"]
-
-        # get clip data from newest build
-        api_path = f"https://medal.tv/_next/data/{build_id}/en/games/{clip_path}.json"
-        async with session.get(api_path) as response:
-            return await response.json()
+        async with session.get(api_url) as response:
+            data = await response.json()
+            return MedalClip(data["contentTitle"], data["contentUrl"])
 
 
-class MyClient(discord.Client):
+class Medal(discord.Client):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
     async def on_message(self, message: discord.Message):
-        # we do not want the bot to reply to itself
-        if message.author == self.user:
-            return
+        if message.author != self.user:
+            for content_id in find_content_ids(message.content):
+                clip = await api_content(content_id)
+                await message.channel.send(
+                    f"{message.author.mention} :magic_wand: **{clip.title}**\n{clip.url}"
+                )
 
-        regex = r"https://medal\.tv/games/([^\s?]*)"
-        matches = re.finditer(regex, message.content)
-        for match in matches:
-            data = await get_medal_clip(match.group(1))
-            if data is None:
-                return print(f"Could not find next data from {match.group(1)}")
+                print(f"Fixed link for {message.author} : {message.content}")
 
-            content_url = data["pageProps"]["clip"]["contentUrl"]
-            content_title = data["pageProps"]["clip"]["contentTitle"]
-
-            await message.reply(
-                f":magic_wand: **{content_title}** {content_url}", mention_author=True
-            )
-
-            print(f"Fixed link for {message.author} : {message.content}")
-
-            try:
-                await asyncio.sleep(1)
-                await message.edit(suppress=True)
-            except discord.errors.Forbidden:
-                print("Tried to suppress message but no permissions :(")
+                try:
+                    await message.delete()
+                except discord.errors.Forbidden:
+                    print("Tried to delete message but no permissions :(")
 
 
-if len(sys.argv) > 1 and sys.argv[1] == "get":
-    try:
-        clip = sys.argv[2]
-        asyncio.run(get_medal_clip(clip))
-    except IndexError:
-        print(
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "get" and len(sys.argv) > 2:
+            for content_id in find_content_ids(sys.argv[2]):
+                return print(asyncio.run(api_content(content_id)).url)
+
+        return print(
             "Usage:\n"
             "   Run bot:    python main.py\n"
             "   Get clip:   python main.py get https://medal.tv/..."
         )
-else:
-    intents = discord.Intents.default()
-    intents.message_content = True
 
-    client = MyClient(intents=intents)
-    client.run(os.environ["DISCORD_TOKEN"])
+    Medal(
+        intents=discord.Intents(
+            message_content=True,
+            guild_messages=True,
+            dm_messages=True,
+        )
+    ).run(os.environ["DISCORD_TOKEN"])
+
+
+if __name__ == "__main__":
+    main()
